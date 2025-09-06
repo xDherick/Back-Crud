a774108eab53633e9f24400881884eacb84ac4b7
Dherick Rodrigues <dherick60@hotmail.com>
2025-09-06 21:05:59 +0000

    Atualizando cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Permite qualquer origem
+    origins "https://meu-condominio-xdhericks-projects.vercel.app"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
